73c7d8f0dc027e0d0bd8d6503dc4445a1c397458
mukong <mukong@oovmi.com>
2019-03-03 18:25:48 +0800

    0303-3

--- app/model/wechat/platforms.go
+++ app/model/wechat/platforms.go
@@ -4,12 +4,12 @@ import "time"
 
 // PlatformsInfo ... 开发平台数据库
 type PlatformsInfo struct {
-	UUID         string    `xorm:"varchar(36) notnull unique 'uuid'"`  // uuid
-	AppID        string    `xorm:"varchar(64) notnull unique 'appid'"` // appid
-	Name         string    `xorm:"varchar(64) 'name'"`                 // 名称
-	AccessToken  string    `xorm:"varchar(64) 'access_token'"`         // 令牌
-	RefreshToken string    `xorm:"varchar(64) 'refresh_token'"`        // 刷新令牌
-	ExpiresIn    int64     `xorm:"varchar(64) 'expires_in'"`           // 令牌过期时间
+	UUID         string    `xorm:"varchar(36) pk notnull unique 'uuid'"`  // uuid
+	AppID        string    `xorm:"varchar(64) pk notnull unique 'appid'"` // appid
+	Name         string    `xorm:"varchar(64) 'name'"`                    // 名称
+	AccessToken  string    `xorm:"varchar(64) 'access_token'"`            // 令牌
+	RefreshToken string    `xorm:"varchar(64) 'refresh_token'"`           // 刷新令牌
+	ExpiresIn    int64     `xorm:"varchar(64) 'expires_in'"`              // 令牌过期时间
 	CreatedAt    int64     `xorm:"created"`
 	UpdatedAt    time.Time `xorm:"updated"`
 	DeletedAt    time.Time `xorm:"deleted"`
